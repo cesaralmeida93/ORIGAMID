e19bfd320ecb885fbbc67129b90d21a15c26f939
César Augusto Santos de Almeida <ocesaralmeida@gmail.com>
2020-01-09 08:53:45 -0300

    adicionada aula de atribuição e ternário

---
+++ javascript_completo_es6/javascript-para-iniciantes/atribuicao-e-ternario/script-aula.js
@@ -0,0 +1,13 @@
+var idade = 20;
+var naoPossuiDiabetes = true;
+
+var podeBeber;
+podeBeber = (idade >= 18 && naoPossuiDiabetes) ? 'Pode beber' : 'Não pode beber';
+
+console.log(podeBeber);
+
+var possuiFaculdade = true;
+if(possuiFaculdade)
+  console.log('Sim possui');
+else
+  console.log('Não possui');
