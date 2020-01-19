f49391ac835ff79dfd9b7714ebc2e7fa8573429d
César Augusto Santos de Almeida <ocesaralmeida@gmail.com>
2020-01-18 21:00:25 -0300

    adiconado exercicio sobre selecao de elementos

---
+++ javascript_completo_es6/selecionar-o-dom/script-aula.js
@@ -0,0 +1,36 @@
+const animais = document.getElementById('animais');
+console.log(animais);
+
+const gridSection = document.getElementsByClassName('grid-section');
+
+console.log(gridSection[1]);
+
+const primeiraLi= document.querySelector('li');
+console.log(primeiraLi);
+
+const primeiraUl= document.querySelector('ul');
+console.log(primeiraUl);
+
+const linkInterno = document.querySelector('[href^="#"]');
+console.log(linkInterno.href);
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg);
+
+const gridSectionHtml = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('.grid-section');
+
+primeiraUl.classList.add('grid-section');
+
+console.log(gridSectionHtml[0]);
+console.log(gridSectionNode[0]);
+
+gridSectionNode.forEach(function(item, index) {
+    console.log(item);
+});
+
+const arrayGrid = Array.from(gridSectionHtml);
+
+arrayGrid.forEach(function(item) {
+    console.log(item);
+});
