4a6341b1d6459db31443a030f0f1155aaec3bd93
César Augusto Santos de Almeida <ocesaralmeida@gmail.com>
2020-02-17 10:36:46 -0300

    finalizada aula de classes e atributos

---
+++ javascript_completo_es6/classes-e-atributos/script.js
@@ -0,0 +1,27 @@
+// Adicione a classe ativo a todos os itens do menu
+const itensMenu = document.querySelectorAll('.menu a');
+
+itensMenu.forEach((item) => {
+    item.classList.add('ativo');
+})
+
+// Remova a classe ativo de todos os itens do menu e 
+// mantenha apenas no primeiro
+itensMenu.forEach((item) => {
+    item.classList.remove('ativo');
+})
+
+itensMenu[0].classList.add('ativo');
+
+// Veriique se as imagens possuem o atributo alt
+const imagens = document.querySelectorAll('img');
+
+imagens.forEach((img) => {
+    const possuiAtributo = img.hasAttribute('alt');
+    console.log(possuiAtributo);
+})
+
+// Modofique o href do link externo no menu
+const link = document.querySelector('a[href^="http"]');
+link.setAttribute('href', 'https://www.google.com/');
+console.log(link);
