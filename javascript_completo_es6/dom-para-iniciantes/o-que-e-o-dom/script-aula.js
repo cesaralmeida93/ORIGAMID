5eb1c31f1ee55b59fb84ce3493aecc42f1eb0d88
cesar almeida <ocesaralmeida@gmail.com>
2020-11-24 07:52:27 -0300

    finalizada aula sobre o que e o DOM

--- javascript_completo_es6/dom-para-iniciantes/o-que-e-o-dom/script-aula.js
+++ javascript_completo_es6/dom-para-iniciantes/o-que-e-o-dom/script-aula.js
@@ -1,18 +1,15 @@
-// window.alert('Isso mesmo');
+// Retorne o url da página atual utilizando o objeto window
+console.log(window.location.href);
 
-const href = window.location.href;
+// Seleciona o primeiro elemento da página que
+//possua a classe ativo
+console.log(document.querySelector('.ativo'));
 
-console.log(href);
+// Retorne a linguagem do navegador
+console.log(navigator.language);
 
-if(href === 'http://127.0.0.1:5500/o-que-e-o-doms/') {
-    console.log('É igual');
-}
+// Retorne a largura da janela
+console.log(window.innerWidth);
 
-const h1Selecionado = document.querySelector('h1');
-const h1Classes = h1Selecionado.classList;
-
-function callBackH1() {
-    console.log('Clicou em', h1Selecionado.innerText);
-}
-
-h1Selecionado.addEventListener('click', callBackH1);
+// Retorne a altura da janela
+console.log(window.innerHeight);
--- javascript_completo_es6/dom-para-iniciantes/o-que-e-o-dom/script.js
+++ javascript_completo_es6/dom-para-iniciantes/o-que-e-o-dom/script.js
@@ -1,16 +1,16 @@
-// Retorne o url da página atual utilizando o objeto window
-const hrefPagina = window.location.href;
-console.log(hrefPagina);
+const href = window.location.href;
 
-// Seleciona o primeiro elemento da página que
-// possua a classe ativo
-const elementoAtivo = document.querySelectorAll('.ativo');
-console.log(elementoAtivo);
+console.log(href);
 
-// Retorne a linguagem do navegador
-const linguagemNavegador = window.navigator.language;
-console.log(linguagemNavegador);
+if (href === 'http://127.0.0.1:5500/o-que-e-o-doms/') {
+    console.log('é igual!');
+}
 
-// Retorne a largura da janela
-const larguraJanela = window.innerWidth;
-console.log(larguraJanela);
+const h1Selecionado = document.querySelector('h1');
+const h1Classes = h1Selecionado.classList;
+
+function teste() {
+    console.log('Clicou em ', h1Selecionado.innerText);
+}
+
+h1Selecionado.addEventListener('click', teste);
