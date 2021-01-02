aed2a91b976e9b98104e2f9e1f06e698c9f346d3
cesar almeida <ocesaralmeida@gmail.com>
2021-01-01 22:27:18 -0300

    atualizada identaçao de arquivos

--- javascript_completo_es6/dom-para-iniciantes/o-que-e-o-dom/script.js
+++ javascript_completo_es6/dom-para-iniciantes/o-que-e-o-dom/script.js
@@ -3,14 +3,14 @@ const href = window.location.href;
 console.log(href);
 
 if (href === 'http://127.0.0.1:5500/o-que-e-o-doms/') {
-    console.log('é igual!');
+  console.log('é igual!');
 }
 
 const h1Selecionado = document.querySelector('h1');
 const h1Classes = h1Selecionado.classList;
 
 function teste() {
-    console.log('Clicou em ', h1Selecionado.innerText);
+  console.log('Clicou em ', h1Selecionado.innerText);
 }
 
 h1Selecionado.addEventListener('click', teste);
